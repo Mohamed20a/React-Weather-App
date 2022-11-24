8e8a3301aa20cf7dc89712adcd591219b43d4c59
Mohamed Ahmed <m16ahmed17@gmail.com>
2022-11-24 21:51:32 +0200

    reat weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+import "./fontawesome.min.css";
+import Weather from "./compontents/Weather";
+import Form from "./compontents/Form";
 
-function App() {
+  //   Key Api--------
+const API_KEY = "50883ea96de0d5a374b6bd4583d75116";
+
+
+class App extends Component {
+
+  state = {
+    Temperature: '',
+    city: '',
+    country: '',
+    humidity: '',
+    description: '',
+    error: ''
+  }
+
+
+  //    Fetch api---------
+  getWeather =  async (e) => {
+    e.preventDefault()
+    const city = e.target.elements.city.value;
+    // const country = e.target.elements.country.value;
+    const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+    const data = await  api.json();
+    if (city) {
+      this.setState({
+        Temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+        
+      })
+    } else{
+      this.setState({
+        Temperature: '',
+        city: '',
+        country: '',
+        humidity: '',
+        description: '',
+        error: 'Please Enter The City ...! '
+      })
+    }
+  }
+
+  render() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="form-container" >
+          <div className="head">
+              <h2>React <span className="color">Weather</span> App</h2>
+              <img className="cloud" src="https://ayushkul.github.io/react-weather-app/icons/perfect-day.svg"></img>
+              <br></br>
+          </div>
+
+          <Form getWeather={this.getWeather}/>
+
+          <Weather 
+          Temperature= {this.state.Temperature}
+          city= {this.state.city}
+          country= {this.state.country}
+          humidity= {this.state.humidity}
+          description= {this.state.description}
+          error= {this.state.error}
+          />
+        
+      </div> 
+
+      <footer>
+      <p> Made By : <a class="link" href="https://portfolioo-1.netlify.app" target="_blank">Mohamed Ahmed Elgeady ♡</a></p>
+      </footer>
     </div>
   );
+  }
 }
 
 export default App;
